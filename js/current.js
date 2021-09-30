ae2d0887b4842d683695bc936fd05183af30aca3
bayer <bayerowatson@gmail.com>
2021-09-30 11:21:51 -0400

    fixed automatically show details on current and new pages

--- js/current.js
+++ js/current.js
@@ -1,43 +1,28 @@
 import { renderWeather } from "./renderWeather.js";
-
 const container = document.querySelector('.weather');
-
-
-
-
 
 window.addEventListener("DOMContentLoaded", () => {
     
         let weather_section = document.createElement('div');
-        
         container.appendChild(weather_section);
-        weather_section.innerHTML = `
-            <div class="weather-content"></div>
-            <button class="button">see more...</button>
-        `;
-
+        weather_section.innerHTML = '<div class="weather-content"></div>';
         let weather_content = weather_section.querySelector(".weather-content");
 
         navigator.geolocation.getCurrentPosition(position => {
             let details = position.coords;
             let gps = details.latitude + ',' + details.longitude;
-            console.log(gps);
             renderWeather(`https://api.weather.gov/points/${gps}`, weather_content)
+                .then(() => {
+                    let weather_details = weather_content.querySelector(".weather-details");
+                    weather_details.classList.add("show");
+                })
                 .catch(err => alert(err.message));
+            
         });
 
         
-        let button = weather_section.querySelector(".button");
-        button.onclick = () => {
-            let weather_details = weather_content.querySelector(".weather-details");
-            weather_details.classList.toggle("show");
-            if (button.innerHTML == "see more...") {
-                button.innerHTML = "see less...";
-            }
-            else {
-                button.innerHTML = "see more..."
-            }
-        };
+
+    
    
 });
 
--- js/new.js
+++ js/new.js
@@ -5,11 +5,20 @@ const container = document.querySelector('.weather');
 
 form.addEventListener('submit', async (e) => {
     e.preventDefault();
-    container.innerHTML = "<p>Loading...</p>";
+   
+    let weather_section = document.createElement('div');
+    container.appendChild(weather_section);
+    weather_section.innerHTML = '<div class="weather-content"></div>';
+    let weather_content = weather_section.querySelector(".weather-content");
+
+
     let gps = null;
     gps = await geocoder(form.gps.value);
-    console.log(gps);
-    renderWeather(`https://api.weather.gov/points/${gps}`, container)
+    renderWeather(`https://api.weather.gov/points/${gps}`, weather_content)
+        .then(() => {
+            let weather_details = weather_content.querySelector(".weather-details");
+            weather_details.classList.add("show");
+        })    
         .catch(err => {
             alert(err.message + '\nMake sure you entered a valid location');
             form.gps.value='';
